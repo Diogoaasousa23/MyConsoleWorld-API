cab0113d5090160507484bb6fff26e1c7c94fe47
Diogoaasousa23 <diogoaadesousa@gmail.com>
2024-06-13 09:51:12 +0100

    .

--- routes/privado.js
+++ routes/privado.js
@@ -2,7 +2,7 @@ const privadoRouter = require('express').Router();
 
 
 // Define uma rota para a página HTML
-privadoRouter.get('/gerirJogos', (req, res) => {
+privadoRouter.get('/gerirGames', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
     res.sendFile('C:/Users/diogo/Documents/ERSC/2ºano/PW/html/MyConsoleWorld-API/templates/backOffice/tabelaCarros.html');
   });
