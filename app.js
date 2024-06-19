4bf7a1b9f6a2494b12a21bb371e7a80f35e2fa69
Diogoaasousa23 <diogoaadesousa@gmail.com>
2024-06-19 17:59:15 +0100

    Diogo

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const privadoRouter = require('./routes/privado.js'); // Caminho ajustado para routes/privado
+const publicoRouter = require('./routes/publico.js');
 
 // Servir arquivos estáticos da pasta templates
 app.use(express.static(path.join(__dirname, 'templates')));
+app.use(express.static(path.join(__dirname, 'Assets')));
 
 // Usar o router
 app.use('/privado.js', privadoRouter);
+app.use('/publico.js', publicoRouter);
+
 
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => {
--- routes/publico.js
+++ routes/publico.js
@@ -1,11 +1,11 @@
-const publicoRouter = require('express').Router();
+const express = require('express');
+const path = require('path');
+const publicoRouter = express.Router();
 
-
-// Define uma rota para a página HTML
+// Rota para /index
 publicoRouter.get('/index', (req, res) => {
-    // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile("C:/Users/diogo/Documents/ERSC/2ºano/PW/html/MyConsoleWorld-API/templates/frontEnd/index.html");
-  });
-
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'index.html'));
+});
 
 module.exports = publicoRouter;
