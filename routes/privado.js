f3eb3c4dfe7dfe3a700695e5b0c9e443d65c1238
Diogoaasousa23 <diogoaadesousa@gmail.com>
2024-06-19 20:36:23 +0100

    Rotas_Done

--- routes/privado.js
+++ routes/privado.js
@@ -7,9 +7,34 @@ router.get('/gerirSony', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'tabelaSony.html'));
 });
 
+// Rota para /privado/gerirMicrosoft
+router.get('/gerirMicrosoft', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'tabelaXbox.html'));
+});
+
+// Rota para /privado/gerirNintendo
+router.get('/gerirNintendo', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'tabelaNintendo.html'));
+});
+
+// Rota para /privado/gerirSteam
+router.get('/gerirSteam', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'tabelaSteam.html'));
+});
+
+// Rota para /privado/gerirPCGames
+router.get('/gerirPCGames', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'tabelaPCGames.html'));
+});
+
 // Rota para /privado/Dashboard
 router.get('/Dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'Dashboard.html'));
+});
+
+// Rota para /privado/Dashboard
+router.get('/manageprofile', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'templates', 'backOffice', 'Manage_profile.html'));
 });
 
 module.exports = router;
--- routes/publico.js
+++ routes/publico.js
@@ -8,4 +8,30 @@ publicoRouter.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'index.html'));
 });
 
+publicoRouter.get('/login', (req, res) => {
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'Login.html'));
+});
+
+publicoRouter.get('/signup', (req, res) => {
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'Sign.html'));
+});
+
+publicoRouter.get('/recoverpassword', (req, res) => {
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'Pass.html'));
+});
+
+publicoRouter.get('/privacypolicy', (req, res) => {
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'privacy-policy.html'));
+});
+
+publicoRouter.get('/termsofservice', (req, res) => {
+  // Envie o arquivo HTML como resposta para a solicitação HTTP
+  res.sendFile(path.join(__dirname, '..', 'templates', 'frontEnd', 'terms-of-service.html'));
+});
+
+
 module.exports = publicoRouter;
